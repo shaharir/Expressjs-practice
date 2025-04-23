4cd29b03608ac3afffd78c63552457f277f231db
sumon <shaharir.cse@gmail.com>
2025-04-23 23:13:59 +0600

    [Feat]: implement new concept for post method

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
 const useRoute = require("./route");
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
 app.use("/api/user", useRoute);
 app.use((req, res) => {
   res.send("ohhhhhh. you lost!");
--- route.js
+++ route.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
 router.get("/", (req, res) => {
-  res.send("Who am I!");
+  res.status(200).json({
+    name: "sumon",
+    statusCode: 200,
+  });
 });
+// Post Method
+router.post("/", (req, res) => {
+  const name = req.body.name;
+  res.send(`Welcome ${name}`);
+});
+// Query params
 router.get("/home", (req, res) => {
-  res.send("Who am I!pppppp");
+  const { id, name } = req.query;
+  res.send(`Who am I!${name}${id}`);
+});
+// Route Params
+router.get("/about/user/:id/name/:name", (req, res) => {
+  const { id, name } = req.params;
+  res.send(`Who am I!${id}${name}`);
+});
+router.get("/about", (req, res) => {
+  const { id, name } = req.params;
+  res.send(`Who am I!${id}${name}`);
 });
 module.exports = router;
